e08a8e397ea18c55dedaa36468c254545cfe6931
Angelo Cardellicchio <a.cardellicchio@gmail.com>
2023-10-27 14:49:36 +0200

    Squashed commit of the following:
    commit c80a16f95a3854a6923bf6b760e4a8ede443ccfc
    Author: Angelo Cardellicchio <a.cardellicchio@gmail.com>
    Date:   Fri Oct 27 14:49:02 2023 +0200
    
        (update) added privacy policy
    
    commit 807542349c75659686f47fb0689db3ed41885e67
    Author: Angelo Cardellicchio <angelo.cardellicchio@sstlab.it>
    Date:   Wed Jul 13 16:35:40 2022 +0200
    
        (update) slight update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,135 +1,11 @@
 import * as React from "react"
 
-// styles
-// const pageStyles = {
-//   color: "#232129",
-//   padding: 96,
-//   fontFamily: "-apple-system, Roboto, sans-serif, serif",
-// }
-// const headingStyles = {
-//   marginTop: 0,
-//   marginBottom: 64,
-//   maxWidth: 320,
-// }
-// const headingAccentStyles = {
-//   color: "#663399",
-// }
-// const paragraphStyles = {
-//   marginBottom: 48,
-// }
-// const codeStyles = {
-//   color: "#8A6534",
-//   padding: 4,
-//   backgroundColor: "#FFF4DB",
-//   fontSize: "1.25rem",
-//   borderRadius: 4,
-// }
-// const listStyles = {
-//   marginBottom: 96,
-//   paddingLeft: 0,
-// }
-// const listItemStyles = {
-//   fontWeight: 300,
-//   fontSize: 24,
-//   maxWidth: 560,
-//   marginBottom: 30,
-// }
-
-// const linkStyle = {
-//   color: "#8954A8",
-//   fontWeight: "bold",
-//   fontSize: 16,
-//   verticalAlign: "5%",
-// }
-
-// const docLinkStyle = {
-//   ...linkStyle,
-//   listStyleType: "none",
-//   marginBottom: 24,
-// }
-
-// const descriptionStyle = {
-//   color: "#232129",
-//   fontSize: 14,
-//   marginTop: 10,
-//   marginBottom: 0,
-//   lineHeight: 1.25,
-// }
-
-// const docLink = {
-//   text: "Documentation",
-//   url: "https://www.gatsbyjs.com/docs/",
-//   color: "#8954A8",
-// }
-
-// const badgeStyle = {
-//   color: "#fff",
-//   backgroundColor: "#088413",
-//   border: "1px solid #088413",
-//   fontSize: 11,
-//   fontWeight: "bold",
-//   letterSpacing: 1,
-//   borderRadius: 4,
-//   padding: "4px 6px",
-//   display: "inline-block",
-//   position: "relative",
-//   top: -2,
-//   marginLeft: 10,
-//   lineHeight: 1,
-// }
-
-// // data
-// const links = [
-//   {
-//     text: "Tutorial",
-//     url: "https://www.gatsbyjs.com/docs/tutorial/",
-//     description:
-//       "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-//     color: "#E95800",
-//   },
-//   {
-//     text: "How to Guides",
-//     url: "https://www.gatsbyjs.com/docs/how-to/",
-//     description:
-//       "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-//     color: "#1099A8",
-//   },
-//   {
-//     text: "Reference Guides",
-//     url: "https://www.gatsbyjs.com/docs/reference/",
-//     description:
-//       "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-//     color: "#BC027F",
-//   },
-//   {
-//     text: "Conceptual Guides",
-//     url: "https://www.gatsbyjs.com/docs/conceptual/",
-//     description:
-//       "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-//     color: "#0D96F2",
-//   },
-//   {
-//     text: "Plugin Library",
-//     url: "https://www.gatsbyjs.com/plugins",
-//     description:
-//       "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-//     color: "#8EB814",
-//   },
-//   {
-//     text: "Build and Host",
-//     url: "https://www.gatsbyjs.com/cloud",
-//     badge: true,
-//     description:
-//       "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-//     color: "#663399",
-//   },
-// ]
-
 const pageStyles = {
   margin: 0,
   padding: 0,
   height: '100%',
   overflow: 'hidden',
+  color: '#fff'
 }
 
 const contentStyle = {
@@ -137,9 +13,19 @@ const contentStyle = {
   left: 0,
   right: 0,
   bottom: 0,
-  top: '0px'
+  top: '0px',
+  // overflow: 'hidden',
+  // display: 'inline-block',
 }
 
+const iframeStyle = {
+  // width: "100%",
+  // height: "100%"
+  marginTop: '-30',
+  marginBottom: '-100',
+  marginLeft: '-100px',
+  marginRight: '-100px'
+}
 
 // markup
 const IndexPage = () => {
@@ -148,10 +34,11 @@ const IndexPage = () => {
       <title>TipToBe</title>
       <div style={contentStyle}>
         <iframe
+          // style={iframeStyle}
           src="https://docs.google.com/presentation/d/e/2PACX-1vQ6S4dwYYb_eTyKpQAHsy8FJOAQs-1SOQvIhCX7PKxx8ihHYl_MeG1dXxfhL7qg6A/embed?start=true&loop=true&delayms=60000&amp;rm=minimal"
-          frameborder="0"
-          width="100%"
-          height="100%"
+          frameborder="-10"
+          width="1280"
+          height="1024"
           allowfullscreen="true"
           mozallowfullscreen="true"
           webkitallowfullscreen="true" />
